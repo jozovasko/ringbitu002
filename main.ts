19a76aa79958645abc65220ac00de2cb773e0bcc
jozovasko <jozovasko@gmail.com>
2025-10-19 21:19:00 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    while (false) {
+        vzdialenost = RingbitCar.ringbitcar_sonarbit(RingbitCar.Distance_Unit.Distance_Unit_cm)
+        if (vzdialenost < 15) {
+            RingbitCar.running_distance(RingbitCar.Direction_run.backward, 5)
+        } else if (vzdialenost > 15) {
+            RingbitCar.running_distance(RingbitCar.Direction_run.forward, 5)
+        } else {
+            RingbitCar.brake()
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    RingbitCar.brake()
+})
+let vzdialenost = 0
+basic.showString("U")
+basic.forever(function () {
+	
+})
